157ad8d5c801bd10425755b1cb28997bef1a4a29
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-08-29 23:28:03 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+(() => {
+  const listItems = document.querySelectorAll('.item');
+
+  console.log(`Number of categories: ${listItems.length}`);
+
+  listItems.forEach((item) => {
+    const categoryName = item.firstElementChild.textContent;
+    const itemCount = item.lastElementChild.childElementCount;
+    console.log(`Category: ${categoryName}\nElements: ${itemCount}`);
+  });
+})();
