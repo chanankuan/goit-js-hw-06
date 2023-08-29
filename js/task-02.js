e0a2519c82758923a939e34edbe2ac59fe603aed
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-08-29 23:28:06 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+(() => {
+  const list = document.getElementById('ingredients');
+  const ingredients = [
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
+  ];
+
+  const markup = [];
+
+  ingredients.forEach((ingredient) => {
+    const li = document.createElement('li');
+    li.classList.add('item');
+    li.textContent = ingredient;
+    markup.push(li.outerHTML);
+  });
+
+  list.insertAdjacentHTML('beforeend', markup.join(''));
+})();
