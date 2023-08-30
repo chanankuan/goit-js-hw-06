32201fb7954956598266d92db5eb26631016e1da
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-08-30 11:38:30 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,12 @@
   const input = document.getElementById('name-input');
   const output = document.getElementById('name-output');
 
-  input.addEventListener('input', () => {
-    input.value === ''
+  const onInput = (e) => {
+    let inputValue = e.currentTarget.value;
+
+    inputValue === ''
       ? (output.textContent = 'Anonymous')
       : (output.textContent = input.value);
-  });
+  };
+  input.addEventListener('input', onInput);
 })();
