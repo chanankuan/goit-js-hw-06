61c9d75afc9d017bd91b98629379236d86bfe2c4
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-08-30 20:12:19 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,39 +1,39 @@
-(() => {
-  const boxes = document.getElementById('boxes');
-  const controlArea = document.getElementById('controls');
-  const input = controlArea.firstElementChild;
-  const createBtn = input.nextElementSibling;
-  const destroyBtn = controlArea.lastElementChild;
-  let size = 30;
+const boxes = document.getElementById('boxes');
+const controlArea = document.getElementById('controls');
+const input = controlArea.firstElementChild;
+const createBtn = input.nextElementSibling;
+const destroyBtn = controlArea.lastElementChild;
+let initialSize = 30;
 
-  createBtn.addEventListener('click', () => createBoxes(input.value));
-  destroyBtn.addEventListener('click', destroyBoxes);
+createBtn.addEventListener('click', () => createBoxes(input.value));
+destroyBtn.addEventListener('click', destroyBoxes);
 
-  // Функція для створення колекції. Можна додавати безкінечно кількість разів.
-  function createBoxes(amount) {
-    const markupArray = [];
-    let i = 0;
+// Функція для створення колекції. Можна додавати безкінечно кількість разів.
+function createBoxes(amount) {
+  const markupArray = [];
+  const lastEl = boxes.lastElementChild;
+  let size = lastEl ? Number(lastEl.dataset.size) + 10 : initialSize;
 
-    for (let i = 0; i < amount; i += 1) {
-      size += 10;
-      markupArray.push(`
-        <div style="width:${size}px; height:${size}px; background-color:${getRandomHexColor()}"></div>
+  for (let i = 0; i < amount; i += 1) {
+    markupArray.push(`
+        <div data-size="${size}" style="width:${size}px; height:${size}px; background-color:${getRandomHexColor()}"></div>
       `);
-    }
 
-    boxes.insertAdjacentHTML('beforeend', markupArray.join(''));
+    size += 10;
   }
 
-  // Функція для очищення колекції
-  function destroyBoxes() {
-    boxes.innerHTML = '';
-    size = 30;
-  }
+  boxes.insertAdjacentHTML('beforeend', markupArray.join(''));
+}
 
-  // Функція для генерації рандомного кольору
-  function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, 0)}`;
-  }
-})();
+// Функція для очищення колекції
+function destroyBoxes() {
+  boxes.innerHTML = '';
+  initialSize = 30;
+}
+
+// Функція для генерації рандомного кольору
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
