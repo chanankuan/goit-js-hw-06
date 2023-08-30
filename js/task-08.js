6f8fc60497beb2f5d8ef2b9b272dc45cc3893f62
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-08-30 20:12:14 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
 
+const onSubmit = (event) => {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+
+  if (!email.value || !password.value) {
+    return alert('Усі поля повинні бути заповнені');
+  }
+
+  return {
+    email: email.value,
+    password: password.value,
+  };
+};
+
+form.addEventListener('submit', onSubmit);
