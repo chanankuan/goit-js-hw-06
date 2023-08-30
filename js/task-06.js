565a14af2355db4dc5da7b543a2b53d8176f7187
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-08-30 20:12:10 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,17 @@
-(() => {
-  const input = document.getElementById('validation-input');
+const input = document.getElementById('validation-input');
 
-  const onBlur = ({ currentTarget }) => {
-    const length = currentTarget.dataset.length;
-    const inputLength = currentTarget.value.length;
-    const classes = currentTarget.classList;
+const onBlur = ({ currentTarget }) => {
+  const length = currentTarget.dataset.length;
+  const inputLength = currentTarget.value.length;
+  const classes = currentTarget.classList;
 
-    if (inputLength == length) {
-      classes.add('valid');
-      classes.remove('invalid');
-    } else {
-      classes.add('invalid');
-      classes.remove('valid');
-    }
-  };
+  if (inputLength == length) {
+    classes.add('valid');
+    classes.remove('invalid');
+  } else {
+    classes.add('invalid');
+    classes.remove('valid');
+  }
+};
 
-  input.addEventListener('blur', onBlur);
-})();
+input.addEventListener('blur', onBlur);
