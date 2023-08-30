3349507e76d729473dbe103012cd14aaaf5c7ee7
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-08-30 20:12:06 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,23 +1,21 @@
-(() => {
-  const decrementButton = document.querySelector('[data-action=decrement]');
-  const incrementButton = document.querySelector('[data-action=increment]');
-  const display = document.querySelector('#value');
-  let counterValue = 0;
+const decrementButton = document.querySelector('[data-action=decrement]');
+const incrementButton = document.querySelector('[data-action=increment]');
+const display = document.querySelector('#value');
+let counterValue = 0;
 
-  const increment = () => {
-    counterValue += 1;
-    updateDisplay(counterValue);
-  };
+const increment = () => {
+  counterValue += 1;
+  updateDisplay(counterValue);
+};
 
-  const decrement = () => {
-    counterValue -= 1;
-    updateDisplay(counterValue);
-  };
+const decrement = () => {
+  counterValue -= 1;
+  updateDisplay(counterValue);
+};
 
-  const updateDisplay = (value) => {
-    display.textContent = value;
-  };
+const updateDisplay = (value) => {
+  display.textContent = value;
+};
 
-  decrementButton.addEventListener('click', decrement);
-  incrementButton.addEventListener('click', increment);
-})();
+decrementButton.addEventListener('click', decrement);
+incrementButton.addEventListener('click', increment);
