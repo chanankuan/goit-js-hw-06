9b914ada2f5822357a58ce62c091834da7bfaba3
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-08-30 20:12:16 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const displayColor = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
+button.addEventListener('click', changeColor);
+
+function changeColor() {
+  const pickColor = getRandomHexColor();
+  document.body.style.backgroundColor = pickColor;
+  displayColor.textContent = pickColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
